57d1b0811e6a1536af8893801104f93f9826b890
nunosilva <nunosilvacmj@hotmail.com>
2020-10-14 22:06:58 +0100

    Start first picture apperance test

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/player/Player.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/player/Player.java
@@ -1,8 +1,16 @@
 package org.academiadecodigo.gnunas.unicorns_and_stuff.player;
 
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Player {
 
-    private PlayerMovement action;
+    //Trying out stuff
+    private PlayerMovement direction;
+    private Picture[] pictures;
+    private final int NUMBER_OF_PICTURES = 8;
 
-    //TODO player logic
+    public Player() {
+        pictures = new Picture[NUMBER_OF_PICTURES];
+    }
+
 }
