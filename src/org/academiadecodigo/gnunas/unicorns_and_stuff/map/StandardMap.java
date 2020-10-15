f7f35800d003d7752914fa6091ab459c9de7c6c4
soyelraul <edupedro1996@gmail.com>
2020-10-15 20:41:46 +0100

    added background to map

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/map/StandardMap.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/map/StandardMap.java
@@ -1,4 +1,12 @@
 package org.academiadecodigo.gnunas.unicorns_and_stuff.map;
 
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class StandardMap implements Map {
+    public Picture picture;
+
+    public StandardMap() {
+        picture = new Picture(0, 0, "resources/background.jpg");
+        picture.draw();
+    }
 }
