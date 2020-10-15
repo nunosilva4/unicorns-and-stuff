03f590620ded37ec0ef42e458afe38502c4538a9
soyelraul <edupedro1996@gmail.com>
2020-10-15 20:41:23 +0100

    Changed player logic

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/player/Player.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/player/Player.java
@@ -6,11 +6,14 @@ public class Player {
 
     //Trying out stuff
     private PlayerMovement direction;
-    private Picture[] pictures;
+    private Picture[] sprites;
     private final int NUMBER_OF_PICTURES = 8;
 
-    public Player() {
-        pictures = new Picture[NUMBER_OF_PICTURES];
+    public Player(int x, int y) {
+        sprites = new Picture[NUMBER_OF_PICTURES];
+
+        sprites[0] = new Picture(x, y, "resources/unicorn.png");
+        sprites[0].draw();
     }
 
 }
