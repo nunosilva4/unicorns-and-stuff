bd8c4cef15e7bf588d8dedf510dba786d306e89a
nunosilva <nunosilvacmj@hotmail.com>
2020-10-15 09:32:15 +0100

    Start ArrayList implementation for player movement

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/input/Handler.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/input/Handler.java
@@ -2,12 +2,20 @@ package org.academiadecodigo.gnunas.unicorns_and_stuff.input;
 
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+import java.util.ArrayList;
+
 import static org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent.*;
 
 public class Handler implements KeyboardHandler {
 
+    private ArrayList<MovementType> playerOneMovement;
+    private ArrayList<MovementType> playerTwoMovement;
+
     //TODO finish constructor and implement keybindings and behaviour
     public Handler() {
+        playerOneMovement = new ArrayList<>();
+        playerTwoMovement = new ArrayList<>();
     }
 
     @Override
@@ -15,7 +23,7 @@ public class Handler implements KeyboardHandler {
 
         switch (keyboardEvent.getKey()) {
             case KEY_UP:
-                //TODO register
+                playerOneMovement.add(MovementType.UP);
                 break;
             case KEY_DOWN:
                 //TODO register
