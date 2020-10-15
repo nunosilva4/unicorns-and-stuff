9376ae045ff0eb63615777d6c51c4183ba699c6e
soyelraul <edupedro1996@gmail.com>
2020-10-15 20:41:15 +0100

    Changed game logic

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/Game.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/Game.java
@@ -3,13 +3,21 @@ package org.academiadecodigo.gnunas.unicorns_and_stuff;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.map.Map;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.map.MapFactory;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.map.MapType;
+import org.academiadecodigo.gnunas.unicorns_and_stuff.player.Player;
 
 public class Game {
 
     private Map map;
 
+    private Player[] players;
+
     public Game(MapType mapType) {
         map = MapFactory.makeMap(mapType);
+
+        players = new Player[2];
+
+        players[0] = new Player(50,50);
+        players[1] = new Player(200, 200);
     }
 
     //TODO everything else
