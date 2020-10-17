9d85e6cbaa78557d161bdb244cfd92f76d2d6467
Luís Moreira <luis_borges@live.com.pt>
2020-10-17 20:46:22 +0100

    add shoot behaviour, add projectile list, add update projectile behaviour, change getX and getY methods to obtain central position

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/player/Player.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/player/Player.java
@@ -1,27 +1,40 @@
 package org.academiadecodigo.gnunas.unicorns_and_stuff.player;
 
+import org.academiadecodigo.gnunas.unicorns_and_stuff.Game;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.input.Direction;
 import org.academiadecodigo.simplegraphics.graphics.Ellipse;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Set;
 
 public class Player {
 
     //Trying out stuff
     private String name;
+
     private Set<Direction> movement;
     private Direction lastDirection;
+
+    // TODO change to Game class?
+    private LinkedList<Projectile> projectiles;
+
     private Picture currentSprite;
     private String[] imagePath;
 
     public Player(String name, Set<Direction> movement, Picture startingSprite, String[] imagePath) {
-        this.imagePath = imagePath;
-        this.movement = movement;
         this.name = name;
 
+        this.movement = movement;
+        if (this.name == "Unicorn") {this.lastDirection = Direction.LEFT;}
+        if (this.name == "Nazicorn") {this.lastDirection = Direction.RIGHT;}
+
+        projectiles = new LinkedList<>();
+
+        this.imagePath = imagePath;
         currentSprite = startingSprite;
+
         currentSprite.draw();
     }
 
@@ -57,15 +70,28 @@ public class Player {
         }
     }
 
-    public void attack() {
-        Projectile projectile = new Projectile(getX(), getY(), 10, lastDirection);
+    public void shoot() {
+        projectiles.add(new Projectile(getX(), getY(), 10, lastDirection));
+    }
+
+    //TODO Understand what would be the best place for this method, change to Game class?
+    public void updateProjectile() {
+        for (Projectile projectile : projectiles) {
+            if(projectile.getX() >= Game.WIDTH || projectile.getX() <= 0) {
+                projectiles.remove(projectile);
+                projectile.remove();
+                continue;
+            }
+
+            projectile.move();
+        }
     }
 
     public int getX() {
-        return currentSprite.getX();
+        return currentSprite.getX() + currentSprite.getWidth() / 2;
     }
 
     public int getY() {
-        return currentSprite.getY();
+        return currentSprite.getY() + currentSprite.getHeight() / 2;
     }
 }
