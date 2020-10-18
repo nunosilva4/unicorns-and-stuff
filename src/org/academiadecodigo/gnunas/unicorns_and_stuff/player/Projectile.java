5d3c6f72a6d1a4b87567c334311c574a7680f747
soyelraul <edupedro1996@gmail.com>
2020-10-18 16:48:32 +0100

    Remove unused properties, add player property, add check collision in move, add some getters

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/player/Projectile.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/player/Projectile.java
@@ -1,57 +1,77 @@
 package org.academiadecodigo.gnunas.unicorns_and_stuff.player;
 
+import org.academiadecodigo.gnunas.unicorns_and_stuff.Game;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.input.Direction;
 import org.academiadecodigo.simplegraphics.graphics.Ellipse;
 
+import java.util.LinkedList;
+
 public class Projectile {
-    private Ellipse projectile;
-    private int x;
-    private int y;
+    private Ellipse projectileSprite;
     private int damageAmount;
     private Direction direction;
 
-    public Projectile(int x, int y, int damageAmount, Direction direction) {
-        this.x = x;
-        this.y = y;
+    private Player player;
+
+    public Projectile(int x, int y, int damageAmount, Direction direction, Player player) {
         this.damageAmount = damageAmount;
         this.direction = direction;
-        projectile = new Ellipse(x, y, 10, 10);
 
-        projectile.fill();
+        this.player = player;
+
+        projectileSprite = new Ellipse(x, y, 10, 10);
+        projectileSprite.fill();
     }
 
     // TODO add different velocity
     public void move() {
+        if (getX() - getWidth() >= Game.WIDTH || getX() <= 0) {
+            remove(player.getProjectilesList());
+        }
+
+        if (getY() - getHeight() >= Game.HEIGHT || getY() <= 0) {
+            remove(player.getProjectilesList());
+        }
+
         switch (direction) {
             case UP:
-                projectile.translate(0, -1);
+                projectileSprite.translate(0, -1);
                 break;
             case DOWN:
-                projectile.translate(0, 1);
+                projectileSprite.translate(0, 1);
                 break;
             case LEFT:
-                projectile.translate(-1, 0);
+                projectileSprite.translate(-1, 0);
                 break;
             case RIGHT:
-                projectile.translate(1, 0);
+                projectileSprite.translate(1, 0);
                 break;
         }
     }
 
+    // TODO Erase from memory
+    public void remove(LinkedList<Projectile> projectiles) {
+        projectiles.remove(this);
+        projectileSprite.delete();
+    }
+
     public int getX() {
-        return projectile.getX();
+        return projectileSprite.getX();
     }
 
     public int getY() {
-        return projectile.getY();
+        return projectileSprite.getY();
+    }
+
+    public int getWidth() {
+        return projectileSprite.getWidth();
+    }
+
+    public int getHeight() {
+        return projectileSprite.getHeight();
     }
 
     public int getDamageAmount() {
         return damageAmount;
-    }
-
-    // TODO Erase from memory
-    public void remove() {
-        projectile.delete();
     }
 }
