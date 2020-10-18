ce8c21b5c4a1b1c4d76449b90551f2901b9d3421
ThaKushi <edupedro1996@gmail.com>
2020-10-18 22:44:54 +0100

    Move createNewStuff to Game class, remove some unwanted properties

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/map/StandardMap.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/map/StandardMap.java
@@ -5,28 +5,14 @@ import org.academiadecodigo.gnunas.unicorns_and_stuff.object.GameObject;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.object.StuffFactory;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.object.StuffType;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.player.Player;
-import org.academiadecodigo.gnunas.unicorns_and_stuff.Game;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class StandardMap implements Map {
 
-    private Picture picture;
-    private Player[] players;
-    private int numberOfObjects;
+    private final Picture picture;
 
-    public StandardMap(Player[] players, int numberOfObjects) {
+    public StandardMap() {
         picture = new Picture(Game.PADDING, Game.PADDING, "resources/rsz_cenas.png");
         picture.draw();
-        this.numberOfObjects = numberOfObjects;
-
-        this.players = players;
-
-        StuffFactory stuffFactory = new StuffFactory();
-        for (int i = 0; i < numberOfObjects; i++) {
-            GameObject gameObject = stuffFactory.creatNewStuff(StuffType.TRAP, MapType.STANDARD, (int) (Math.random() * Game.WIDTH), (int) (Math.random() * Game.HEIGHT), players);
-            if (gameObject != null) {
-                gameObject.show();
-            }
-        }
     }
 }
