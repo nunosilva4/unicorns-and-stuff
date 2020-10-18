fe3e3f028e472c2ac749a065a792eb3cfbbef824
ThaKushi <edupedro1996@gmail.com>
2020-10-18 22:43:44 +0100

    Remove arguments from method makeMap and from new instance StandardMap

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/map/MapFactory.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/map/MapFactory.java
@@ -1,14 +1,12 @@
 package org.academiadecodigo.gnunas.unicorns_and_stuff.map;
 
-import org.academiadecodigo.gnunas.unicorns_and_stuff.player.Player;
-
 public class MapFactory {
 
-    public static Map makeMap(MapType mapType, Player[] players) {
+    public static Map makeMap(MapType mapType) {
 
         switch (mapType) {
             case STANDARD:
-                return new StandardMap(players, 10);
+                return new StandardMap();
             default:
                 //TODO continue implementation when there are new maps
                 throw new IllegalArgumentException();
