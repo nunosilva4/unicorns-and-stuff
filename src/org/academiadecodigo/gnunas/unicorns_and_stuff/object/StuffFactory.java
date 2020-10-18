665e8479ddc10b8a096b77922319823146f93a6c
ThaKushi <edupedro1996@gmail.com>
2020-10-18 22:46:10 +0100

    Convert createNewStuff method to static, add some static properties to class, implement behaviour to keep track of spawned entities/stuff on game

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/StuffFactory.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/StuffFactory.java
@@ -1,15 +1,23 @@
 package org.academiadecodigo.gnunas.unicorns_and_stuff.object;
 
-import org.academiadecodigo.gnunas.unicorns_and_stuff.map.MapType;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.player.Player;
 
 public class StuffFactory {
 
-    public GameObject creatNewStuff(StuffType stuffType, MapType mapType, int x, int y, Player[] players) {
+    public static final int MAXSTUFF = 10;
+    private static int currentNumberOfStuff;
+
+    public static GameObject createNewStuff(StuffType stuffType, int x, int y, Player[] players) {
+
+        if (currentNumberOfStuff >= MAXSTUFF) {
+            return null;
+        }
+
+        currentNumberOfStuff++;
 
         switch (stuffType) {
             case TRAP:
-                return new TrapStuff(x, y, mapType, players);
+                return new TrapStuff(x, y, players);
             case SHIT:
                 break;
             case METEORS:
