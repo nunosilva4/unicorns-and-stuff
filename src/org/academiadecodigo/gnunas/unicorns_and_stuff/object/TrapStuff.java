f8bbb1e0caf75e9517bfa451b4967208b652e9a6
ThaKushi <edupedro1996@gmail.com>
2020-10-19 22:23:41 +0100

    Update to hit method

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/TrapStuff.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/TrapStuff.java
@@ -1,23 +1,21 @@
 package org.academiadecodigo.gnunas.unicorns_and_stuff.object;
 
+import org.academiadecodigo.gnunas.unicorns_and_stuff.Game;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.player.Player;
 
 
 public class TrapStuff extends GameObject {
 
-    private final int DAMAGE = 10;
-
     public TrapStuff(int x, int y, Player[] players) {
-        super(x, y, players);
+        super(x, y);
     }
 
     public void hit() {
-        for (Player player : getPlayers()){
-            if (!player.isDead()){
-                player.hit(DAMAGE);
+        for (Player player : Game.getPlayers()) {
+            if (isHitting(player)) {
+
+                return;
             }
         }
     }
-
-
 }
