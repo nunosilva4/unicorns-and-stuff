e4dff5bdea31a69e334ba9c3384e421c7fbb2176
ThaKushi <edupedro1996@gmail.com>
2020-10-19 22:23:03 +0100

    Remove unused property and add new isHitting method

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/GameObject.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/GameObject.java
@@ -1,5 +1,6 @@
 package org.academiadecodigo.gnunas.unicorns_and_stuff.object;
 
+import org.academiadecodigo.gnunas.unicorns_and_stuff.Game;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.input.Direction;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.map.Map;
 import org.academiadecodigo.gnunas.unicorns_and_stuff.map.MapType;
@@ -12,12 +13,9 @@ import java.util.TimerTask;
 public abstract class GameObject {
 
     private final Rectangle rectangle;
-    private final Player[] players;
 
-    public GameObject(int x, int y, Player[] players) {
+    public GameObject(int x, int y) {
         rectangle = new Rectangle(x, y, 25, 25);
-
-        this.players = players;
     }
 
     public void show() {
@@ -28,12 +26,24 @@ public abstract class GameObject {
         rectangle.delete();
     }
 
+    public boolean isHitting(Player player) {
+        int playerBodyMaxX = player.getX() + player.getWidth() / 2;
+        int playerBodyMinX = player.getX() - player.getWidth() / 2;
+        int playerBodyMaxY = player.getY() + player.getHeight() / 2;
+        int playerBodyMinY = player.getY() - player.getHeight() / 2;
+
+        return (getX() - (getWidth() / 2)) + (getWidth() / 2) + Game.PADDING <= playerBodyMaxX &&
+                (getX() - (getWidth() / 2)) + (getWidth() / 2) + Game.PADDING >= playerBodyMinX &&
+                (getY() - (getHeight() / 2)) + (getHeight() / 2) + Game.PADDING <= playerBodyMaxY &&
+                (getY() - (getHeight() / 2)) + (getHeight() / 2) + Game.PADDING >= playerBodyMinY;
+    }
+
     public int getX() {
-        return rectangle.getX();
+        return rectangle.getX() + getWidth() / 2;
     }
 
     public int getY() {
-        return rectangle.getY();
+        return rectangle.getY() + getHeight() / 2;
     }
 
     public int getHeight() {
@@ -42,9 +52,5 @@ public abstract class GameObject {
 
     public int getWidth() {
         return rectangle.getWidth();
-    }
-
-    public Player[] getPlayers() {
-        return players;
     }
 }
