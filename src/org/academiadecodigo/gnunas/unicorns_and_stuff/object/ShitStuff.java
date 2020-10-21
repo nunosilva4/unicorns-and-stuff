0d18654e73681fe52961062649887731e945616d
ThaKushi <edupedro1996@gmail.com>
2020-10-21 01:30:53 +0100

    Add some logic

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/ShitStuff.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/ShitStuff.java
@@ -5,9 +5,29 @@ import org.academiadecodigo.gnunas.unicorns_and_stuff.player.Player;
 
 public class ShitStuff extends GameObject {
 
-    public ShitStuff(int x, int y) {
-        super(x, y, "resources/Objects/shit.png");
+    public ShitStuff(int x, int y, String string) {
+        super(x, y, string);
     }
 
+    @Override
+    public Player check() {
+        Player player = super.check();
+        if (player == null) {
+            return null;
+        }
 
+        player.setSliding(true);
+
+        return player;
+    }
+
+    @Override
+    public void delete() {
+        Player player = super.check();
+        if (player != null) {
+            player.setSliding(false);
+        }
+
+        super.delete();
+    }
 }
