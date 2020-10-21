d0a06dcb3ab82a2c877a8bd3e6de6ef0fafb718a
ThaKushi <edupedro1996@gmail.com>
2020-10-21 01:32:29 +0100

    Add some logic

--- src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/TrapStuff.java
+++ src/org/academiadecodigo/gnunas/unicorns_and_stuff/object/TrapStuff.java
@@ -5,7 +5,29 @@ import org.academiadecodigo.gnunas.unicorns_and_stuff.player.Player;
 
 public class TrapStuff extends GameObject {
 
-    public TrapStuff(int x, int y) {
-        super(x, y, "resources/Objects/trap.png");
+    public TrapStuff(int x, int y, String string) {
+        super(x, y, string);
+    }
+
+    @Override
+    public Player check() {
+        Player player = super.check();
+        if (player == null) {
+            return null;
+        }
+
+        player.setStunned(true);
+
+        return player;
+    }
+
+    @Override
+    public void delete() {
+        Player player = super.check();
+        if (player != null) {
+            player.setStunned(false);
+        }
+
+        super.delete();
     }
 }
